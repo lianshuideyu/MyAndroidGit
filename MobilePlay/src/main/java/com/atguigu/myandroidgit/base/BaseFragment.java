6c005c0fa81558f5e5d240555b68340ee3b5aa47
lianshuideyu <834893479@qq.com>
2017-05-18 15:54:03 +0800

    定义基类

---
+++ MobilePlay/src/main/java/com/atguigu/myandroidgit/base/BaseFragment.java
@@ -0,0 +1,61 @@
+package com.atguigu.myandroidgit.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/5/18.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    private Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+
+    }
+
+    /**
+     * 当系统要创建 Fragment的视图的时候回调该方法
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return initView();//加载子类布局
+    }
+
+    //抽象方法子类必须继承
+    public abstract View initView();
+
+    /**
+     *当Activity创建成功的时候回调该方法
+     * 初始化数据
+     * 联网请求
+     * 绑定数据
+     * @param savedInstanceState
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    /*联网请求
+    绑定数据时重写该方法*/
+    public void initData(){
+
+    }
+
+}
