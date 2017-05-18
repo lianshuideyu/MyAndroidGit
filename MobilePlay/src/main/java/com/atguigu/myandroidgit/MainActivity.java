09753614e5266f04a5fdf11b69d687c9412bfc45
lianshuideyu <834893479@qq.com>
2017-05-18 10:28:11 +0800

    2秒后进入主页面

--- MobilePlay/src/main/java/com/atguigu/myandroidgit/MainActivity.java
+++ MobilePlay/src/main/java/com/atguigu/myandroidgit/MainActivity.java
@@ -1,16 +1,55 @@
 package com.atguigu.myandroidgit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //2秒后进入主页面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        },2000);
     }
 
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MobilePlayMainActivity.class);
+        startActivity(intent);
+        //关闭当前页面
+        finish();
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+        //当有触摸事件的时候直接进入主页面
+        startMainActivity();
+
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);//如果传入参数为null所有消息被移除
+    }
 }
---
+++ MobilePlay/src/main/java/com/atguigu/myandroidgit/MobilePlayMainActivity.java
@@ -0,0 +1,13 @@
+package com.atguigu.myandroidgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MobilePlayMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_play_main);
+    }
+}
