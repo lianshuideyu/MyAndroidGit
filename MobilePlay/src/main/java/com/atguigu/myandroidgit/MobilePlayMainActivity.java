5d9f6336ba2415ba6f70aab66ea681643dcf6fbf
lianshuideyu <834893479@qq.com>
2017-05-19 22:06:55 +0800

    播放器项目三练习，完成，Fragment之间的切换

--- MobilePlay/src/main/java/com/atguigu/myandroidgit/MobilePlayMainActivity.java
+++ MobilePlay/src/main/java/com/atguigu/myandroidgit/MobilePlayMainActivity.java
@@ -1,7 +1,6 @@
 package com.atguigu.myandroidgit;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioGroup;
@@ -14,16 +13,11 @@ import com.atguigu.myandroidgit.fragment.NetVideoFragment;
 
 import java.util.ArrayList;
 
-import static com.atguigu.myandroidgit.R.id.fl_main_content;
-
 public class MobilePlayMainActivity extends AppCompatActivity  {
     private RadioGroup rg_bottom_button;
+    private int position;
     private ArrayList<BaseFragment> fragments;
-
-    private int position;//记录Fragment页码
-
-    private Fragment currentFragment;
-    private Fragment temFragment;//缓冲的Fragment
+    private BaseFragment temFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +26,22 @@ public class MobilePlayMainActivity extends AppCompatActivity  {
 
         rg_bottom_button = (RadioGroup)findViewById(R.id.rg_bottom_button);
 
+        initData();
 
-        //添加集合数据
-        initFragment();
-        //RadioGroup的点击事件监听
-        initListener();
-
+        CheckedChangeListener();
 
     }
 
-    //RadioGroup的点击事件监听
-    private void initListener() {
+    private void initData() {
+        fragments = new ArrayList<>();
+
+        fragments.add(new LocalVideoFragment());
+        fragments.add(new LocalAudioFragment());
+        fragments.add(new NetAudioFragment());
+        fragments.add(new NetVideoFragment());
+    }
+
+    private void CheckedChangeListener() {
         rg_bottom_button.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -60,53 +59,43 @@ public class MobilePlayMainActivity extends AppCompatActivity  {
                         position = 3;
                         break;
                 }
-                currentFragment = fragments.get(position);//得到当前的Fragment页面
-                
-                switchFragment();
+
+                BaseFragment currentFragment = fragments.get(position);
+                addFragment(currentFragment);
             }
         });
-
-        //默认勾选第本地视频个位置，这个要放在RadioGroup的监听事件之后，保证达到页面也随之改变的效果
+        //设置初进页面默认的勾选
         rg_bottom_button.check(R.id.rb_video);
     }
 
-    private void switchFragment() {
-        
+    private void addFragment(BaseFragment currentFragment) {
         if(temFragment != currentFragment) {
             //开启事务
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            //切换
-            if(currentFragment != null) {
-
-                if(!currentFragment.isAdded()) {//如果没有添加
-                    //将之前的隐藏
-                    if(temFragment != null) {
-                        ft.hide(temFragment);
-                    }
-                    //如果没有添加就添加
-                    ft.add(fl_main_content,currentFragment);
-                }else {//添加过了
-                    //先把之前的隐藏
-                    if(temFragment != null) {
-                        ft.hide(temFragment);
-                    }
-
-                    //如果添加就显示
-                    ft.show(currentFragment);
-
+            if(!currentFragment.isAdded()) {
+                //没有添加的时候
+                if(temFragment != null) {
+                    ft.hide(temFragment);
                 }
-                ft.commit();//不要忘了提交
+                //隐藏之前的
+                //添加当前的
+                ft.add(R.id.fl_main_content,currentFragment);
+            }else {
+                //添加过的时候
+                if(temFragment != null) {
+                    ft.hide(temFragment);
+                }
+                //隐藏之前的
+                //显示当前的
+                ft.show(currentFragment);
             }
-            temFragment = currentFragment;//重新赋值
+
+            ft.commit();
+
+            temFragment = currentFragment;
         }
+
     }
 
-    private void initFragment() {
-        fragments = new ArrayList<>();
-        fragments.add(new LocalVideoFragment());
-        fragments.add(new LocalAudioFragment());
-        fragments.add(new NetAudioFragment());
-        fragments.add(new NetVideoFragment());
-    }
 
 }
